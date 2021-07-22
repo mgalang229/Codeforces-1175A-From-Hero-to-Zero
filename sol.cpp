3df7ffc378592b8a5f2258ee92927cd4ee18a493
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-07-22 14:44:08 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		long long n, k;
+		cin >> n >> k;
+		long long cnt = 0;
+		while (n > 0) {
+			if (n % k == 0) {
+				// if 'n' is divisible by 'k', then simply divide it
+				n /= k;
+				// increment 'cnt'
+				cnt++;
+			} else {
+				// add the remainder of 'n' and 'k' and add it to 'cnt'
+				cnt += (n % k);
+				// subtract the remainder of 'n' and 'k' from 'n' itself (to make it divisible by 'k')
+				n -= (n % k);
+			}
+		}
+		cout << cnt << '\n';
+	}
+	return 0;
+}
